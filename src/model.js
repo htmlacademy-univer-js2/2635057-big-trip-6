8579c3a784b70cb9e5d2bff26c0ab58b705be8d5
nana9 <dovgomelaanastasia@gmail.com>
2026-05-25 22:55:06 +0500

    Большие перемены (часть 1)

--- src/model.js
+++ src/model.js
@@ -1,11 +1,22 @@
+import { DESTINATIONS } from './const.js';
 import { generatePoints } from './mock/point.js';
 
 export default class Model {
+  #points = [];
+
   constructor() {
-    this.points = generatePoints(5);
+    this.#points = generatePoints(5);
   }
 
   getPoints() {
-    return this.points;
+    return this.#points;
+  }
+
+  getDestinations() {
+    return DESTINATIONS;
+  }
+
+  updatePoint(updatedPoint) {
+    this.#points = this.#points.map((point) => point.id === updatedPoint.id ? updatedPoint : point);
   }
 }
